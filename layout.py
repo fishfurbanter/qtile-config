5aa182d0ee6f0af14f0976adf6443047268bb58b
Fishfur Banter <fishfurbanter@gmail.com>
2017-07-05 13:18:23 +0300

    changed layout.py, added floating layout

--- layout.py
+++ layout.py
@@ -5,5 +5,6 @@ layouts = [
     layout.Max(),
     layout.Tile(name="terminals", border_focus="#000000"),
     layout.Stack(num_stacks=2, border_focus='#383c4a'),
-    layout.TreeTab()
+    layout.TreeTab(),
+    layout.Floating(),
 ]
